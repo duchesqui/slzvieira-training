47c4ccd453bdfbb29e201bb0ad4f12a23b86fd86
slzvieira <slzvieira@gmail.com>
2012-06-11 03:52:11 +0000

    JDBC: Transações e Consultas (ResultSet)

--- 2-Java-Programmer-Modulo-II/Capitulo11/2.table.sql
+++ 2-Java-Programmer-Modulo-II/Capitulo11/2.table.sql
@@ -14,3 +14,11 @@ ALTER TABLE tab_func
 ADD CONSTRAINT fk_role_func
     FOREIGN KEY (role_code)
         REFERENCES tab_role (role_code);
+
+CREATE TABLE tab_foto(
+    user_code      INTEGER PRIMARY KEY AUTO_INCREMENT,
+    user_name      VARCHAR(30) NOT NULL,
+    user_pict      LONGBLOB,
+    orig_file_name VARCHAR(50)
+);
+
