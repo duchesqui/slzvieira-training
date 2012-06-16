328bf3d77e838b76f3b721cbcad9aa89fad52144
slzvieira <slzvieira@gmail.com>
2012-06-16 19:24:46 +0000

    Ajuste dos scripts SQL.

---
+++ 2-Java-Programmer-Modulo-II/ProjetoFinal/scripts/3.data.sql
@@ -0,0 +1,10 @@
+INSERT INTO impacta.tab_role (role_name) VALUES ('Analista de Sistemas');
+INSERT INTO impacta.tab_role (role_name) VALUES ('Programador Java');
+INSERT INTO impacta.tab_role (role_name) VALUES ('DBA');
+INSERT INTO impacta.tab_role (role_name) VALUES ('Gerente de Projetos');
+
+INSERT INTO impacta.tab_func (func_name, func_rmnt_val, role_code) VALUES ('Manuel',  3500.5 , 2);
+INSERT INTO impacta.tab_func (func_name, func_rmnt_val, role_code) VALUES ('Joaquim', 2650.15, 3);
+INSERT INTO impacta.tab_func (func_name, func_rmnt_val, role_code) VALUES ('Maria',   4550.85, 1);
+INSERT INTO impacta.tab_func (func_name, func_rmnt_val, role_code) VALUES ('José',    1205.34, 2);
+INSERT INTO impacta.tab_func (func_name, func_rmnt_val, role_code) VALUES ('Ricardo', 6000   , 4);
