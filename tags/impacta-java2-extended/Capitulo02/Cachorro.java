97dee115974bbf8791c10b440329a78a2e8d7758
slzvieira@gmail.com <slzvieira@gmail.com@8f6bcd11-6c73-7295-f09e-9135c0ca4d0c>
2012-06-25 19:30:32 +0000

    Criação da tag "impacta-java2-extended".

---
+++ tags/impacta-java2-extended/Capitulo02/Cachorro.java
@@ -0,0 +1,27 @@
+public class Cachorro {
+
+	private String nome;
+	private String raca;
+
+	public Cachorro(String nome, String raca) {
+		super();
+		this.nome = nome;
+		this.raca = raca;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRaca() {
+		return raca;
+	}
+
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+}
---
+++ tags/impacta-java2-extended/Capitulo02/Gato.java
@@ -0,0 +1,27 @@
+public class Gato {
+
+	private String nome;
+	private Cor cor;
+
+	public Gato(String nome, Cor cor) {
+		super();
+		this.nome = nome;
+		this.cor = cor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Cor getCor() {
+		return cor;
+	}
+
+	public void setCor(Cor cor) {
+		this.cor = cor;
+	}
+}
