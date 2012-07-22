b137dc1d3d6d6f3f6a26a48f854121c3fede2280
slzvieira <slzvieira@gmail.com>
2012-07-22 14:25:10 +0000

    Correções diversas de problemas encontrados durante a aula.

--- 1-Java-Programmer-Modulo-I/Capitulo09/Motorista.java
+++ 1-Java-Programmer-Modulo-I/Capitulo09/Motorista.java
@@ -1,11 +1,26 @@
 public class Motorista extends Funcionario {
-	private String cnh;
-	
-	public String getCnh() {
-		return cnh;
-	}
-	
-	public void setCnh(String cnh) {
-		this.cnh = cnh;
-	}
+    private String cnh;
+    private String placaAutomovel;
+
+    public String getCnh() {
+        return cnh;
+    }
+
+    public void setCnh(String cnh) {
+        this.cnh = cnh;
+    }
+
+    public String getPlacaAutomovel() {
+        return placaAutomovel;
+    }
+
+    public void setPlacaAutomovel(String placaAutomovel) {
+        this.placaAutomovel = placaAutomovel;
+    }
+
+    public String toString() {
+        return super.toString();
+             + "\nCNH      : " + cnh
+             + "\nPlaca    : " + placaAutomovel;
+    }
 }
